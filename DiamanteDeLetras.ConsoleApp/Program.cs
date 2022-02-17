8f7e103e7b7133e33c153e013d0f98eefa1cda89
Tiago <tiagosantiniathayde@outlook.com>
2022-02-17 13:19:55 -0300

    Add project files.

---
+++ DiamanteDeLetras.ConsoleApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DiamanteDeLetras.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int letraA = 'A';
+
+            int meioDoDiamante;
+
+            while (true)
+            {
+                Console.Write("Digite a letra que estará no meio do diamante: ");
+                meioDoDiamante = Console.Read();
+
+                if (meioDoDiamante > 90 || meioDoDiamante < 65)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Por favor, digite um valor entre A e Z (maiúsculo)!");
+                    Console.ReadLine();
+
+                    Console.ResetColor();
+                    continue;
+                }
+                else
+                    break;
+            }
+
+            int tamanhoInicialEspacosExterior = (meioDoDiamante - 1) / 2;
+            int tamanhoInicialEspacosInterior = -1;
+            
+            Console.WriteLine();
+
+            for (int i = letraA; i < meioDoDiamante; i++)
+            {
+                for (int j = 0; j < tamanhoInicialEspacosExterior; j++)
+                    Console.Write(" ");
+
+                char letraAtual = Convert.ToChar(i);
+
+                Console.Write(letraAtual);
+
+                for (int k = 0; k < tamanhoInicialEspacosInterior; k++)
+                    Console.Write(" ");
+
+                if (i != letraA)
+                    Console.Write(letraAtual);
+
+                Console.WriteLine();
+
+                tamanhoInicialEspacosExterior--;
+                tamanhoInicialEspacosInterior += 2;
+            }
+
+            for (int i = meioDoDiamante; i >= letraA; i--)
+            {
+                for (int j = 0; j < tamanhoInicialEspacosExterior; j++)
+                    Console.Write(" ");
+
+                char letraAtual = Convert.ToChar(i);
+
+                Console.Write(letraAtual);
+
+                for (int k = 0; k < tamanhoInicialEspacosInterior; k++)
+                    Console.Write(" ");
+
+                if (i != letraA)
+                    Console.Write(letraAtual);
+
+                Console.WriteLine();
+
+                tamanhoInicialEspacosExterior++;
+                tamanhoInicialEspacosInterior -= 2;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DiamanteDeLetras.ConsoleApp/Program2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamanteDeLetras.ConsoleApp
+{
+    internal class Program2
+    {
+    }
+}
